e98dbf21ba2e534a4d34cb128195b27b3616164f
Josedi O <josediuepb@gmail.com>
2023-03-20 10:56:40 -0300

    styles bug fix

--- src/Components/Formulario/Formulario.js
+++ src/Components/Formulario/Formulario.js
@@ -8,6 +8,7 @@ import {
   CardContent,
   MenuItem,
 } from "@mui/material";
+import "./Formulario.css";
 
 const Formulario = () => {
   return (
@@ -67,87 +68,97 @@ const Formulario = () => {
                   )}
                 </Field>
                 <br />
-                <Field name="status">
-                  {({ field }) => (
-                    <TextField
-                      sx={{ width: "32%" }}
-                      {...field}
-                      label="Staus"
-                      select
-                      margin="dense"
-                      variant="outlined"
-                      error={touched.status && Boolean(errors.status)}
-                      helperText={touched.status && errors.status}
-                    >
-                      <MenuItem value={10}>Concluido</MenuItem>
-                      <MenuItem value={20}>Incompleto</MenuItem>
-                    </TextField>
-                  )}
-                </Field>
-                <Field name="client">
-                  {({ field }) => (
-                    <TextField
-                      sx={{ width: "64%" }}
-                      {...field}
-                      label="Cliente"
-                      select
-                      margin="dense"
-                      variant="outlined"
-                      error={touched.client && Boolean(errors.client)}
-                      helperText={touched.client && errors.client}
-                    >
-                      <MenuItem value={10}>cliente 1</MenuItem>
-                      <MenuItem value={20}>cliente 2</MenuItem>
-                    </TextField>
-                  )}
-                </Field>
-                <Field name="dateOfsell">
-                  {({ field }) => (
-                    <TextField
-                      sx={{ width: "32%" }}
-                      {...field}
-                      label="Data da Venda"
-                      margin="dense"
-                      variant="outlined"
-                      error={touched.dateOfsell && Boolean(errors.dateOfsell)}
-                      helperText={touched.dateOfsell && errors.dateOfsell}
-                    />
-                  )}
-                </Field>
-                <Field name="weight">
-                  {({ field }) => (
-                    <TextField
-                      sx={{ width: "32%" }}
-                      {...field}
-                      label="Quantidade(KG)"
-                      margin="dense"
-                      variant="outlined"
-                      error={touched.weight && Boolean(errors.weight)}
-                      helperText={touched.weight && errors.weight}
-                    />
-                  )}
-                </Field>
-                <Field name="budget">
-                  {({ field }) => (
-                    <TextField
-                      sx={{ width: "32%" }}
-                      {...field}
-                      label="Valor da Venda"
-                      margin="dense"
-                      variant="outlined"
-                      error={touched.budget && Boolean(errors.budget)}
-                      helperText={touched.budget && errors.budget}
-                    />
-                  )}
-                </Field>
-
+                <div className="line2">
+                  <Field name="status">
+                    {({ field }) => (
+                      <TextField
+                        sx={{ width: "32%" }}
+                        {...field}
+                        label="Staus"
+                        select
+                        margin="dense"
+                        variant="outlined"
+                        error={touched.status && Boolean(errors.status)}
+                        helperText={touched.status && errors.status}
+                      >
+                        <MenuItem value={10}>Concluido</MenuItem>
+                        <MenuItem value={20}>Incompleto</MenuItem>
+                      </TextField>
+                    )}
+                  </Field>
+                  <Field name="client">
+                    {({ field }) => (
+                      <TextField
+                        className="cliente"
+                        {...field}
+                        label="Cliente"
+                        select
+                        margin="dense"
+                        variant="outlined"
+                        error={touched.client && Boolean(errors.client)}
+                        helperText={touched.client && errors.client}
+                      >
+                        <MenuItem value={10}>cliente 1</MenuItem>
+                        <MenuItem value={20}>cliente 2</MenuItem>
+                      </TextField>
+                    )}
+                  </Field>
+                </div>
+                <div className="line3">
+                  <Field name="dateOfsell">
+                    {({ field }) => (
+                      <TextField
+                        sx={{ width: "32%" }}
+                        {...field}
+                        label="Data da Venda"
+                        margin="dense"
+                        variant="outlined"
+                        error={touched.dateOfsell && Boolean(errors.dateOfsell)}
+                        helperText={touched.dateOfsell && errors.dateOfsell}
+                      />
+                    )}
+                  </Field>
+                  <Field name="weight">
+                    {({ field }) => (
+                      <TextField
+                        sx={{ width: "33%" }}
+                        {...field}
+                        label="Quantidade(KG)"
+                        margin="dense"
+                        variant="outlined"
+                        error={touched.weight && Boolean(errors.weight)}
+                        helperText={touched.weight && errors.weight}
+                      />
+                    )}
+                  </Field>
+                  <Field name="budget">
+                    {({ field }) => (
+                      <TextField
+                        sx={{ width: "33%" }}
+                        {...field}
+                        label="Valor da Venda"
+                        margin="dense"
+                        variant="outlined"
+                        error={touched.budget && Boolean(errors.budget)}
+                        helperText={touched.budget && errors.budget}
+                      />
+                    )}
+                  </Field>
+                </div>
                 <br />
-                <Button variant="contained" color="secondary">
-                  Voltar
-                </Button>
-                <Button type="submit" variant="contained" color="primary">
-                  Enviar
-                </Button>
+                <div className="btnInputs">
+                  <Button
+                    className="btn"
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                  >
+                    Enviar
+                  </Button>
+                  <Button className="btn" variant="contained" color="secondary">
+                    Voltar
+                  </Button>
+                </div>
               </Form>
             )}
           </Formik>
